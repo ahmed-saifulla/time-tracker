4fac057923939d6ad24de7a664f77d810d8d87f9
ahsaifulla <ahsaifulla@gmail.com>
2022-11-06 01:24:10 +0530

    start and pause stopwatch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
 
   const [liveTime, setTime] = useState("")
+  const [isStart, setIsStart] = useState(false)
+  const [isPause, setIsPause] = useState(false)
+  const [liveStopWatch, setLiveStopWatch] = useState({hh: 0 , mm: 0, ss: 0})
 
   const updateTime = () => {
     let liveDate = new Date()
@@ -25,25 +28,68 @@ function App() {
     
   }
 
+  const updateStopWachTime = () => {
+      let tempLiveStopWatch = liveStopWatch;
+      tempLiveStopWatch.ss += 1;
+      if(tempLiveStopWatch.ss > 60){
+        tempLiveStopWatch.ss = 0
+        tempLiveStopWatch.mm += 1
+      }
+      if(tempLiveStopWatch.mm > 60){
+        tempLiveStopWatch.mm = 0
+        tempLiveStopWatch.hh += 1
+      }
+      setLiveStopWatch(tempLiveStopWatch)
+  }
+
   useEffect(() => {
+
+    const stopwatchInterval = setTimeout(() => {
+      if(isStart && !isPause){
+        updateStopWachTime()
+      }
+    }, 1000);  
+
     const interval = setTimeout(() => {
       updateTime()
     }, 1000);
 
-    return () => clearTimeout(interval);
-   }, [liveTime]);
+    return () => {
+      clearTimeout(interval)
+      clearTimeout(stopwatchInterval)
+    };
+   }, [liveTime, liveStopWatch]);
 
   
   return (
     <div className="App">
       <div className="timer">
-        <div id="MyClockDisplay" className="clock" onload="updateTime()">
+        <div className="clock">
           {liveTime} 
+        </div>
+
+        <div className="clock stopwatch">
+          {liveStopWatch.hh + " : " + liveStopWatch.mm + " : " + liveStopWatch.ss} 
+        </div>
+
+        <div className="buttons">
+          <button className="btn" onClick={() => {
+            setIsStart(true)
+            setIsPause(false)
+            updateStopWachTime()
+          }}>
+            Start
+          </button>
+          <button className="btn" onClick={() => {
+            setIsStart(false)
+            setIsPause(true)
+          }}>Pause</button>
+          <button className="btn">Save</button>
         </div>
       </div>
 
       <div className="tasks">
-        {/* Tasks Here  */}
+        Tasks Here 
       </div>
     </div>
   );
